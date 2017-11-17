22afa6ea3eadeeeb150ce8adf9610cb37559234c
Gabriele Romeo <gabriele.romeo@gmail.com>
2017-11-17 14:57:07 +0100

    Update the Context Object so that it has the total control over the Canvas' system

--- src/Context.js
+++ src/Context.js
@@ -1,6 +1,9 @@
 class Context {
-  constructor($canvasElement) {
-    this.ctx = $canvasElement.getContext('2d');
+  constructor($canvas) {
+    this.canvas = $canvas;
+    this.context = this.canvas.getContext('2d');
+    this.$offScreenCanvas = this.canvas.cloneNode();
+    this.ctx = this.$offScreenCanvas.getContext('2d');
     this.calls = [];
     this.initMethods();
   }
@@ -12,12 +15,17 @@ class Context {
       fillStyle: style => {
         this.ctx.fillStyle = style;
       },
+      font: style => {
+        this.ctx.font = style;
+      },
       lineTo: (x, y) => this.ctx.lineTo(x, y),
       moveTo: (x, y) => this.ctx.moveTo(x, y),
       stroke: () => this.ctx.stroke(),
       rect: (x, y, width, height) => this.ctx.rect(x, y, width, height),
       clearRect: (x, y, width, height) =>
         this.ctx.clearRect(x, y, width, height),
+      fillRect: (x, y, width, height) => this.ctx.fillRect(x, y, width, height),
+      fillText: (txt, x, y, maxWidth) => this.ctx.fillText(txt, x, y, maxWidth),
     };
 
     const scope = this;
@@ -33,6 +41,38 @@ class Context {
       addMethod(name, method);
     });
   }
+
+  clear() {
+    this.clearRect(
+      0,
+      0,
+      this.$offScreenCanvas.width,
+      this.$offScreenCanvas.height,
+    );
+    this.context.clearRect(
+      0,
+      0,
+      this.$offScreenCanvas.width,
+      this.$offScreenCanvas.height,
+    );
+  }
+
+  getContext() {
+    return this;
+  }
+  getCanvas() {
+    return this.$offScreenCanvas;
+  }
+
+  /**
+   * Handles the rendering on a visible canvas. If the argument
+   * @param {HTMLCanvasElement} The resulting canvas
+   * @returns {void}
+   */
+  renderOnCanvas(canvas) {
+    this.context.drawImage(canvas || this.$offScreenCanvas, 0, 0);
+  }
+
   assign(k, v) {
     this.ctx[k] = v;
   }
