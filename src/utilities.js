2dddd8bf187244f13f9d30a3791c344a09e90089
Gabriele Romeo <gabriele.romeo@gmail.com>
2017-11-20 18:28:28 +0100

    Add curry, flip and run

--- src/utilities.js
+++ src/utilities.js
@@ -14,6 +14,50 @@ export const type = x => {
 };
 
 /**
+  * Take a function and returns another function which takes a list of arguments
+  * and applies the first function to the arguments swapped in reverse order.
+  */
+const flip = fn => (...args) => fn.apply(this, args.reverse());
+
+/**
+ * Takes a Function with {N} parameters and splits it in a series of
+ * functions each taking a single argument. It allows you to provide an
+ * optional parameter 'n' which sets the function's arity.
+ * @param {Function} fn A function to be curried
+ * @param {Integer}   n An optional integer representing the arity of the
+ *                      fn function
+ *
+ * @returns {Function} It returns a series of functions each taking a
+ *                     single argument.
+ */
+const curry = (fn, n) => {
+  const arity = n || fn.length;
+
+  return function curried(...args) {
+    const context = this;
+
+    return args.length >= arity
+      ? fn.apply(context, args)
+      : (...remain) => curried.apply(this, args.concat(remain));
+  };
+};
+
+/**
+ * Takes a Function with {N} parameters and splits it in a series of
+ * functions each taking a single argument. It allows you to provide an
+ * optional parameter 'n' which sets the function's arity. Unlike curry,
+ * rcurry curries a function's arguments from right to left.
+ * @param {Function} fn A function to be curried
+ * @param {Integer}   n An optional integer representing the arity of the
+ *                      fn function
+ *
+ * @returns {Function} It returns a series of functions each taking a
+ *                     single argument.
+ *
+ */
+export const run = (fn, n) => curry(flip(fn), n);
+
+/**
  * Executes a shallow copy of the provided source objects into a target object
  */
 export const copy = (t, ...ss) =>
@@ -29,6 +73,15 @@ export const copy = (t, ...ss) =>
  *                            expected one, False otherwise
  */
 export const is = what => item => type(item) === what;
+
+/**
+ * Evaluates if the provided item is defined
+ *
+ * @param      {Any}  item    The item under test
+ * @return    {Boolean}  True if the item is neither Null nor Undefined, False
+ *                            otherwise
+ */
+export const isDefined = item => !is('Null')(item) && !is('Undefined')(item);
 
 /**
  * Evaluates if the provided item satisfies a predicate
@@ -51,3 +104,30 @@ export const evaluate = pred => (d, a) => (pred(d) ? d : a);
  */
 export const getRandomInt = (max = 10, min = 0) =>
   MathFloor(MathRnd() * (max - min)) + min;
+
+/**
+ * Get a random rgb color
+ * @return {String}      A random rgb color in the format
+ *                       rgb([0-255], [0-255], [0-255])
+ */
+export const randomRGB = () => {
+  const r = getRandomInt(256);
+  const g = getRandomInt(256);
+  const b = getRandomInt(256);
+
+  return `rgb(${r},${g},${b})`;
+};
+
+/**
+ * Get a random rgba color
+ * @return {String}      A random rgba color in the format
+ *                       rgba([0-255], [0-255], [0-255], [0-1])
+ */
+export const randomRGBA = () => {
+  const r = getRandomInt(256);
+  const g = getRandomInt(256);
+  const b = getRandomInt(256);
+  const a = Math.random(1.1);
+
+  return `rgba(${r},${g},${b},${a})`;
+};
